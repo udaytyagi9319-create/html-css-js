32d10a687aefc67f3d348bd683a9b53710f6db61
Uday Tyagi <udaytyagi9319@gmail.com>
2026-09-05 00:06:55 +0530

    portfoilo.html

--- try.py
+++ try.py
@@ -1,4 +1,3 @@
-n =int(input("enter a number: "))
-for i in range(1,n + 1):
-                    print(" " * (n - 1), end="")
-                    print(("*")*(2*n-1))
+with open("33_project.html","r") as f:
+ text = f.read()
+ print(text)
